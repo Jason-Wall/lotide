055a1eb71d58cca9ed599290570555825af9410d
Jason-Wall <jason.waldick@gmail.com>
2023-02-06 17:53:41 -0700

    Complete logic for assertEqual, including emojis

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+const assertEqual = function(actual,expected) {
+  if (actual === expected) {
+    console.log(`✅: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
