1bad8f95170f255b3fbb87de58eccf52ed71fb55
Jason-Wall <jason.waldick@gmail.com>
2023-02-07 18:38:25 -0700

    Create flatten.js

---
+++ flatten.js
@@ -0,0 +1,9 @@
+const flatten = function(arr){
+let returnArr = [];
+for (let step of arr){
+  typeof step.isArray ? returnArr.push(...step): returnArr.push(step);
+  }
+  console.log('returnArr', returnArr);
+};
+
+flatten([1, 2, [3, 4], 5, [6]]) // => [1, 2, 3, 4, 5, 6]
