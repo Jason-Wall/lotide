7c75a78d4e10bf573e020e316497265879a41310
Jason-Wall <jason.waldick@gmail.com>
2023-02-13 08:56:11 -0700

    Create testing directory

---
+++ test/headTest.js
@@ -0,0 +1,6 @@
+const assertEqual = require('../assertEqual');
+const head = require('../head');
+
+
+console.log(assertEqual(head([]), undefined));
+console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
