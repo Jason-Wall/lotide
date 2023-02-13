d257c1e1aa2fe14c7a158eaf9088b1e3fb681d75
Jason-Wall <jason.waldick@gmail.com>
2023-02-13 10:38:36 -0700

    refactor for mochaChai from assertEqual

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,24 @@
-const assertEqual = require('../assertEqual');
-const head = require('../head');
+const assert = require('chai').assert;
+const head   = require('../head');
 
-assertEqual(head([]), undefined);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// const assertEqual = require('../assertEqual');
+// const head = require('../head');
+
+// assertEqual(head([]), undefined);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+describe('# Head -  first instance from an array', () => {
+
+  it('returns undefined when passed an empty array', () => {
+    assert.strictEqual(head([]),undefined);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+  
+  it(`returns 'Hello' for ["Hello", "Lighthouse", "Labs"]`, () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), 'Hello');
+  });
+  
+});
