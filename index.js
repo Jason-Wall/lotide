d644c54ca85bbab06807ac14d7003cefea41979a
Jason-Wall <jason.waldick@gmail.com>
2023-02-13 17:15:38 -0700

    create index for functions

---
+++ index.js
@@ -0,0 +1,20 @@
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const countLetters = require('./countLetters');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const countOnly = require('./countOnly');
+
+
+module.exports = {
+  head: head,
+  tail: tail,
+  middle: middle,
+  countLetters:countLetters,
+  letterPositions:letterPositions,
+  map:map,
+  takeUntil:takeUntil,
+  countOnly:countOnly
+};
